3112fae848a18bdb8ee00f26c0d77dc87e41f45c
Alex Jenter <AlexJenter@users.noreply.github.com>
2026-05-17 15:19:42 +0200

    clean up

--- src/routes/blog/(posts)/weighted-voronoi-stippeling/utils.ts
+++ src/routes/blog/(posts)/weighted-voronoi-stippeling/utils.ts
@@ -1,31 +1,6 @@
 // @ts-ignore
 import { Delaunay } from "d3-delaunay";
 
-type Vec2 = { x: number; y: number };
-
-export function add(a: Vec2, b: Vec2): Vec2 {
-  return { x: a.x + b.x, y: a.y + b.y };
-}
-export function sub(a: Vec2, b: Vec2): Vec2 {
-  return { x: a.x - b.x, y: a.y - b.y };
-}
-export function scale(a: Vec2, s: number): Vec2 {
-  return { x: a.x * s, y: a.y * s };
-}
-export function mul(a: Vec2, b: Vec2): Vec2 {
-  return { x: a.x * b.x, y: a.y * b.y };
-}
-export function len(v: Vec2): number {
-  return Math.hypot(v.x, v.y);
-}
-export function normalize(v: Vec2): Vec2 {
-  const l = len(v) || 1;
-  return { x: v.x / l, y: v.y / l };
-}
-export function perp(v: Vec2): Vec2 {
-  return { x: -v.y, y: v.x };
-}
-
 export function applyWeightedCentroid(
   pts: [number, number][],
   lum: Uint8ClampedArray,
